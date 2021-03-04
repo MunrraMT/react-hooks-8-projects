59661f8dab320d5ec3255ee0cc09dfa4ae6e746d
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-03-04 14:56:06 -0400

    19. Criando a interface HTML - parte 1/3

--- calculadora-app/src/calculadora.js
+++ calculadora-app/src/calculadora.js
@@ -1,7 +1,20 @@
 import './calculadora.css';
+import { Jumbotron } from 'react-bootstrap';
 
 function Calculadora() {
-  return <h1>Calculadora</h1>;
+  return (
+    <Jumbotron
+      style={{
+        background: 'transparent !important',
+        backgroundColor: '#007bff',
+        padding: '5px',
+        margin: '5px',
+        width: '400px',
+      }}
+    >
+      <h1>Teste</h1>
+    </Jumbotron>
+  );
 }
 
 export default Calculadora;
