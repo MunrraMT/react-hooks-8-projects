e4dbfa9c98ff157fdf09c0bad7861df2fbe40088
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-03-04 20:29:04 -0400

    24. Implementando o serviço principal da calculadora - parte 1/5

---
+++ calculadora-app/src/calculadora.service.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CalculadoraService from './calculadora.service';
+
+describe('Teste do CalculadoraService', () => {
+  const [calcular] = CalculadoraService();
+});
