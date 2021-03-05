6d0126bafe40e0e45bf4af2ed186bf14eb3b835c
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-03-04 20:39:22 -0400

    25. Implementando o serviço principal da calculadora - parte 2/5

---
+++ calculadora-app/src/calculadora.service.js
@@ -0,0 +1,7 @@
+function CalculadoraService() {
+  function calcular(numero1, numero2, operacao) {
+    return 0;
+  }
+}
+
+export default CalculadoraService;
--- calculadora-app/src/calculadora.service.test.js
+++ calculadora-app/src/calculadora.service.test.js
@@ -4,4 +4,9 @@ import CalculadoraService from './calculadora.service';
 
 describe('Teste do CalculadoraService', () => {
   const [calcular] = CalculadoraService();
+
+  it('Deve garantir que 1 + 4 = 5', () => {
+    let soma = calcular(1, 4, '+');
+    expect(soma).toEqual(5);
+  });
 });
