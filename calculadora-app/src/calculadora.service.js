6f7902020948a4f52a2d7558c5ea4e669c75d91e
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-03-04 21:32:37 -0400

    26. Implementando o serviço principal da calculadora - parte 3/5

--- calculadora-app/src/calculadora.service.js
+++ calculadora-app/src/calculadora.service.js
@@ -1,7 +1,21 @@
 function CalculadoraService() {
+  const SOMA = '+';
+
   function calcular(numero1, numero2, operacao) {
-    return 0;
+    let resultado;
+
+    switch (operacao) {
+      case SOMA:
+        resultado = numero1 + numero2;
+        break;
+
+      default:
+        return 'Operação inválida';
+    }
+
+    return resultado;
   }
+  return [calcular];
 }
 
 export default CalculadoraService;
