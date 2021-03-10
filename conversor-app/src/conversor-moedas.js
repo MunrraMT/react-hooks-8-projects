8ba3f16003fe42d5ab94b74db30b3376e92da7bb
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-03-10 17:04:27 -0400

    43. Criando o formulário HTML - parte 1/4

--- conversor-app/src/conversor-moedas.js
+++ conversor-app/src/conversor-moedas.js
@@ -1,7 +1,15 @@
 import './conversor-moedas.css';
+import { Jumbotron, Button, Form, Col, Spinner } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 function ConversorMoedas() {
-  return <h1>Conversor de moedas em ReactJS</h1>;
+  return (
+    <section>
+      <h1>Conversor de moedas em ReactJS</h1>
+      <Jumbotron></Jumbotron>
+    </section>
+  );
 }
 
 export default ConversorMoedas;
